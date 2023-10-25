5467765648c3bb8333081ddff5abbc10536ae6c5
+ <+>
2023-10-25 19:38:30 -0400

    Modified DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id ="victory";
+newHeader.innerHTML="<h1>YVONNIE BANDA is the champion</h1></p>";
